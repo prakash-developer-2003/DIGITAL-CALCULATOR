edc9b4e5493c90ac6bffa23c91a3597a1a054837
prakash123 <127091879+prkash123@users.noreply.github.com>
2023-10-03 11:26:54 +0530

    DIG-CAL
    this my digital calculator  and using html,css and js.

---
+++ calcu.js
@@ -0,0 +1,80 @@
+
+
+// calculator
+
+
+var add=document.getElementById("add")
+console.log(add);
+
+add.addEventListener("click" ,function(){
+
+    var num1=document.getElementById("num1").value
+
+    var num2=document.getElementById("num2").value
+
+    var sum =Number(num1)+Number(num2)
+
+    var result =document.getElementById("result")
+    result.textContent=`the result of ${num1} + ${num2} is ${sum}`
+
+
+})
+
+
+var sub =document.getElementById("sub")
+
+sub.addEventListener("click",()=>{
+
+    var num1=document.getElementById("num1").value
+
+    var num2=document.getElementById("num2").value
+
+    var sum =Number(num1)-Number(num2)
+
+    var result=document.getElementById("result")
+    result.textContent=`the result of ${num1} +${num2} is ${sum}`
+})
+
+
+
+var multi =document.getElementById("multi")
+
+multi.addEventListener("click",()=>{
+
+    var num1=document.getElementById("num1").value
+
+    var num2=document.getElementById("num2").value
+
+    var sum =Number(num1)*Number(num2)
+
+    var result=document.getElementById("result")
+    result.textContent=`the result of ${num1} +${num2} is ${sum}`
+})
+
+
+var divi =document.getElementById("divi")
+
+divi.addEventListener("click",()=>{
+
+    var num1=document.getElementById("num1").value
+
+    var num2=document.getElementById("num2").value
+
+    var sum =Number(num1)/Number(num2)
+
+    var result=document.getElementById("result")
+    result.textContent=`the result of ${num1} +${num2} is ${sum}`
+})
+
+
+
+var mod=document.getElementById("mod")
+
+mod.addEventListener("click",()=>{
+    var num1=document.getElementById("num1").value
+    var num2=document.getElementById("num2").value
+    var sum = Number(num1)%Number(num2)
+
+    var result = document.getElementById("result")
+    result.textContent=`the result of ${num1} + ${num2} is ${sum}`
+})
